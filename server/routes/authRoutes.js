acee371fecb6a7472d06f2d299b39537a4efbc93
Andriy Polukhin <andriy.polukhin@gmail.com>
2019-03-25 19:41:21 +0200

    header

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -13,12 +13,14 @@ module.exports = app => {
 		})
 	);
 
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+		res.redirect('/surveys');
+	});
 
 	// Logout
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send(req.user);
+		res.redirect('/');
 	});
 
 	// 2. Looking for a user
